406bcdcc84a561106ade15a605a105ab2211be59
nitin-dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2019-02-12 16:42:13 +0530

    updates...

--- models/user.js
+++ models/user.js
@@ -5,8 +5,6 @@ mongoose.Promise = global.Promise;
 var User  = mongoose.model('User',{
   expoToken :{
     type:String,
-    unique : true,
-    required:true,
     minlength:20,
     trim: true
   },
