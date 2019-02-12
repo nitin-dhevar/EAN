fb29550daae2d8ebd4115c91e1e041809e738f49
nitin-dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2019-02-12 15:46:29 +0530

    updates...

--- models/user.js
+++ models/user.js
@@ -16,6 +16,12 @@ var User  = mongoose.model('User',{
     minlength:1,
     trim: true    //removes whitespaces from start and end
   },
+  dept:{
+    type:String,
+    required:true, //text is required
+    minlength:1,
+    trim: true    //removes whitespaces from start and end
+  },
   batch:{
     type:String,
     required:true, //text is required
