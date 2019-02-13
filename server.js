a07146ccd367db6ac41c7c71bd8a96f99981b733
Amey Deshpande <ameyd30@gmail.com>
2019-02-13 15:13:16 +0530

    CROS ADDED

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 port = process.env.PORT || 3000 ;
 // var checkPost = [];
 var app = express();
+var cors = require('cors');
 var hbs  = require('hbs');
+app.use(cors());
 app.set('view engine', 'hbs');
 app.get('/', function (req, res) {
     res.render('maintenance');
