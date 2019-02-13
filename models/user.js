c3138b4a1b37f293d43fe1cff7204b6ac70c360e
nitin-dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2019-02-14 00:40:09 +0530

    updates..

--- models/user.js
+++ models/user.js
@@ -5,63 +5,59 @@ mongoose.Promise = global.Promise;
 var User  = mongoose.model('User',{
   expoToken :{
     type:String,
-    minlength:1,
     trim: true,
     default: 'null'
   },
   sub:{
     type:String,
-    required:true, //text is required
-    minlength:1,
     unique:true,
     trim: true    //removes whitespaces from start and end
   },
   branch:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+     //text is required
     trim: true    //removes whitespaces from start and end
   },
   batch:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   year:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   division:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   regId:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   fName:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true  
   },
    lName:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true  
   }, 
   email:{
     type:String,
-    required:true, //text is required
-    minlength:1,
+    //required:true, //text is required
+    //minlength:1,
     trim: true  
   }
   
