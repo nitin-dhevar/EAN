042801279e0cf795ceab0149d55e4734e85e55aa
nitin-dhevar <46418326+nitin-dhevar@users.noreply.github.com>
2019-02-13 20:55:15 +0530

    update...

--- models/user.js
+++ models/user.js
@@ -5,32 +5,33 @@ mongoose.Promise = global.Promise;
 var User  = mongoose.model('User',{
   expoToken :{
     type:String,
-    //minlength:1,
+    minlength:1,
     trim: true,
     default: 'null'
   },
   sub:{
     type:String,
     required:true, //text is required
-    //minlength:1,
+    minlength:1,
+    unique:true,
     trim: true    //removes whitespaces from start and end
   },
   branch:{
     type:String,
     required:true, //text is required
-    //minlength:1,
+    minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   batch:{
     type:String,
     required:true, //text is required
-    //minlength:1,
+    minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   year:{
     type:String,
     required:true, //text is required
-    //minlength:1,
+    minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   division:{
@@ -42,7 +43,7 @@ var User  = mongoose.model('User',{
   regId:{
     type:String,
     required:true, //text is required
-    //minlength:1,
+    minlength:1,
     trim: true    //removes whitespaces from start and end
   },
   fName:{
